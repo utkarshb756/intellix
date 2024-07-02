d974924a56881d1861fa06b398c80bafa158e77b
utkarshb756 <116302051+utkarshb756@users.noreply.github.com>
2024-07-02 17:06:18 +0530

    Update layout.tsx

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -2,17 +2,21 @@ import type { PropsWithChildren } from "react";
 
 const LandingLayout = ({ children }: PropsWithChildren) => {
   return (
-    <main className="h-full overflow-hidden relative">
-      <video
-        autoPlay
-        loop
-        muted
-        className="absolute inset-0 w-full h-full object-cover"
-      >
-        <source src="/background-video.mp4" type="video/mp4" />
-        {/* Add additional source tags for different video formats if needed */}
-      </video>
-      <div className="relative mx-auto max-w-screen-xl h-full">{children}</div>
+    <main className="relative">
+      <div className="fixed inset-0 z-0">
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute inset-0 w-full h-full object-cover"
+        >
+          <source src="/background-video.mp4" type="video/mp4" />
+          {/* Add additional source tags for different video formats if needed */}
+        </video>
+      </div>
+      <div className="relative z-10 mx-auto max-w-screen-xl min-h-screen">
+        {children}
+      </div>
     </main>
   );
 };
