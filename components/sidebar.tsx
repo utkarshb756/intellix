b017ba73c7aced22bf81cbb4f1d9d8496864990c
utkarshb756 <116302051+utkarshb756@users.noreply.github.com>
2024-07-07 09:48:43 +0530

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -27,7 +27,7 @@ export const Sidebar = ({ apiLimitCount = 0, isPro = false }: SidebarProps) => {
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-          <div className="relative w-12 h-12 mr-4">
+          <div className="relative w-8 h-8 mr-4">
             <Image src="/logo.png" alt="Genius logo" fill />
           </div>
 
