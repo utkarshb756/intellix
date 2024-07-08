d3483484c841edc2ef74c3841dbcb1eb6426d466
utkarshb756 <brbbhl756@gmail.com>
2024-07-08 21:00:51 +0530

    changes in page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,6 +2,7 @@ import { LandingContent } from "@/components/landing-content";
 // import { LandingFooter } from "@/components/landing-footer";
 import { LandingHero } from "@/components/landing-hero";
 import { LandingNavbar } from "@/components/landing-navbar";
+import TestimonialCarousel from "@/components/review";
 
 const LandingPage = () => {
   return (
